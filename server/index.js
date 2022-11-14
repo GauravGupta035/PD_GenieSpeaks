23b5b5528a11c09bb0854b954ad57e19a3470a5d
Shivansh Pandey <shivansh.spandey@gmail.com>
2022-11-14 10:24:42 +0530

    FlipkartSpider: Product price bug fix

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const app = require("./server.js");
 
 const PORT = process.env.SERVER_PORT || 5000;
-const MONGO_URI = process.env.MONGO_ATLAS_URI || "mongodb://localhost:27017/GenieSpeaksFlipTest"
+const MONGO_URI = process.env.MONGO_ATLAS_URI || "mongodb://localhost:27017/GenieSpeaks"
 
 console.log("Connecting to DB...");
 
